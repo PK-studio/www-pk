480ea5d69c90bbf5a0600f5d697a0692df1f9ad3
PK-studio <vb.karwowski@gmail.com>
2016-09-20 21:29:28 +0100

    After brake
    Correct style,
    Correct mobile brake point
    Footer

--- js/pk.js
+++ js/pk.js
@@ -133,7 +133,7 @@ $(document).ready(function(){
 	});	
 });
 
-// CHANGE COLOR BACKGROUND PARALEX 
+// HEADER - CHANGE COLOR BACKGROUND PARALEX & MARGIN TEXT
 $(document).ready(function(){
 	var header = $('#section1');
 	var content = $('.container-s1');
@@ -145,46 +145,49 @@ $(document).ready(function(){
 	  
 		var scrollTop = $(this).scrollTop();
 		var calc = 0 + (scrollTop/900) ;
-
+		
 		if ( calc > '1' ) {
 		  calc = 1;
 		} else if ( calc < '0' ) {
 		  calc = 0;
 		}
+		
 		var calc2 = 475 + (scrollTop/2) ;
 		
+		// change text position
 		content.css({ 'margin-top': '' + calc2 + 'px'});
-		// This is quick way
-		//header.css({ 'background': 'rgba(255, 255, 255, ' + calc + ') none repeat scroll 0% 0% / auto padding-box border-box' });
-		
-		// good way
+
+		// change color - good way
 		var newColor = 'rgba(255,255,255,' + calc + ')';
 		var newBackground = background.replace(pattern, newColor);
 		console.log(newBackground); 
 		header.css({ 'background': newBackground });
+		
+		// change color - quick way
+		//header.css({ 'background': 'rgba(255, 255, 255, ' + calc + ') none repeat scroll 0% 0% / auto padding-box border-box' });
 	});
 });
 
-$(document).ready(function(){
-// CHANGE ICON ON 'TIMELINE'
 
-//job1
+// 'TIMELINE' - CHANGE ICON ON
+$(document).ready(function(){
+	/*job1*/
 	$('#job1').on('shown.bs.collapse', function () {
 	   $("#jobicon1").removeClass("glyphicon-menu-down hvr-sink").addClass("glyphicon-menu-up hvr-float");
 	});
-
 	$('#job1').on('hidden.bs.collapse', function () {
 	   $("#jobicon1").removeClass("glyphicon-menu-up hvr-float").addClass("glyphicon-menu-down hvr-sink");
 	});
-//job2
+	
+	/*job2*/
 	$('#job2').on('shown.bs.collapse', function () {
 	   $("#jobicon2").removeClass("glyphicon-menu-down hvr-sink").addClass("glyphicon-menu-up hvr-float");
 	});
-
 	$('#job2').on('hidden.bs.collapse', function () {
 	   $("#jobicon2").removeClass("glyphicon-menu-up hvr-float").addClass("glyphicon-menu-down hvr-sink");
 	});
-//job3
+	
+	/*job3*/
 	$('#job3').on('shown.bs.collapse', function () {
 	   $("#jobicon3").removeClass("glyphicon-menu-down hvr-sink").addClass("glyphicon-menu-up hvr-float");
 	});
@@ -193,6 +196,20 @@ $(document).ready(function(){
 	   $("#jobicon3").removeClass("glyphicon-menu-up hvr-float").addClass("glyphicon-menu-down hvr-sink");
 	});
 });
+
+/* 'TIMELINE' - close one scroll
+$(document).ready(function(){
+	var changenavigation = $('#section4').offset().top ;
+	
+	$(window).on('scroll',function(){
+
+		stop = Math.round($(window).scrollTop());
+		if (stop >= changenavigation) {
+			$("#job1, #job2, #job3").removeClass("customCollapse collapse in").addClass("customCollapse collapse");
+			$("#jobicon1, #jobicon2, #jobicon3").removeClass("glyphicon-menu-up hvr-float").addClass("glyphicon-menu-down hvr-sink");
+		}
+	});	
+});*/
 
 //Initiat elements animation - WOW JS - (class="wow fadeInDown")
 jQuery(function($){
@@ -226,5 +243,32 @@ $(function() {
             $(document).unbind('scroll');
         }
     });
-    
+	
+});
+
+//Parallax 2 - change color background
+$(document).ready(function(){
+	var header = $('.bg-quotation-cover');
+	var background = header.css('background');
+	var pattern = /rgba\(\d{1,3},\s?\d{1,3},\s?\d{1,3},\s?(\d{1,3}?(\.?\d{1,}){0,1})\)/i;
+	
+		
+	$(window).on('scroll', function () {
+	  
+		var scrollTop = $(this).scrollTop();
+		var position = $('#section6').offset().top ;
+		//var calc = 0.45 + ((3000-scrollTop)/1950) ;
+		var calc = 0.65 - ((scrollTop - position + 550)/1200) ;
+		
+		if ( calc > '1' ) {
+		  calc = 1;
+		} else if ( calc < '0' ) {
+		  calc = 0;
+		}		
+
+		var newColor = 'rgba(0,0,0,' + calc + ')';
+		var newBackground = background.replace(pattern, newColor);
+		console.log(newBackground); 
+		header.css({ 'background': newBackground });
+	});
 });
